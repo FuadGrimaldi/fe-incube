b6e1d118bfe1995773893c78f45e1279248db65c
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2025-06-04 11:40:05 +0700

    fix: delete authOptions in page route

--- src/app/dashboard/setting/edit-profile/page.tsx
+++ src/app/dashboard/setting/edit-profile/page.tsx
@@ -1,8 +1,5 @@
 // ProfilePage.tsx
 import React from "react";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../../api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
 import { Metadata } from "next";
 import EditProfileForm from "@/components/Profile/Edit-Profile";
 
@@ -13,12 +10,6 @@ export const metadata: Metadata = {
 };
 
 const ProfilePage: React.FC = async () => {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    // Redirect jika sesi tidak ditemukan
-    redirect("/login");
-    return null;
-  }
   return (
     <div className="p-6">
       <div className="h-max-screen space-y-8">
